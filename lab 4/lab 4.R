b0d6745aefff81ba38eee15c30e8ffefb5cc8152
Elsa Chen <pec009@ucsd.edu>
2024-05-21 12:56:23 -0700

    all lab data up to now

---
+++ lab 4/lab 4.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "BIMM 143 lab 4"
+#' author: "Elsa Chen (PID: 16632961)"
+#' date: "4/11/2024"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ = "l", col = "red", lwd = 3)
